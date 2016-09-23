c3cd503f97c9b93a9c619f9ec8c285975a397425
dipak <dipak@PFTBLR-IMAC-004.local>
2016-09-23 19:46:38 +0530

    Added source code

---
+++ SourceCode/PlayerViewController.swift
@@ -0,0 +1,11 @@
+//
+//  PlayerViewController.swift
+//
+//  Created by Dipak on 9/22/16.
+//  Copyright © 2016 Prime Focus Technologies. All rights reserved.
+//
+
+import UIKit
+
+@objc public class PlayerViewController: UIViewController {
+}
---
+++ SourceCode/TestProject/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by dipak on 9/23/16.
+//  Copyright © 2016 Dipak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@class PlayerViewController;
+
+@interface ViewController : UIViewController
+//@interface ViewController : PlayerViewController
+@property(nonatomic, strong) PlayerViewController *test;
+
+
+@end
+
